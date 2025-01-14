03449728816919fe33483e0808bd8dce2e3a0861
angeldiazsems <56977790+angeldiazsems@users.noreply.github.com>
2025-01-13 16:44:54 -0800

    Update robotcontainer.py

--- robotcontainer.py
+++ robotcontainer.py
@@ -2,8 +2,10 @@ import constants
 
 import commands2
 import commands2.button
-
+import subsystems.drivetrain
+import commands.arcadedrive
 import wpilib
+import autoCmd
 
 # from subsystems.drivetrain import arcadeDrive
 # from commands.arcadedrive import ArcadeDrive
@@ -11,12 +13,22 @@ import wpilib
 from subsystems.limelight import limelightSystem
 from commands.limelightdisplay import limelightDisplay
 
+
 class RobotContainer:
     def __init__(self) -> None:
         self.limelight = limelightSystem()
+        self.auto = autoCmd()
         self.stick = wpilib.Joystick(constants.DRIVE_JOYSTICK_PORT)
         self.configureButtonBindings()
+        
 
     def configureButtonBindings(self) -> None:
-        # self.drivetrain.setDefaultCommand(ArcadeDrive(self.drivetrain, lambda: self.stick.getX(), lambda: self.stick.getY()))
+        #self.drivetrain.setDefaultCommand(ArcadeDrive(self.drivetrain, lambda: self.stick.getX(), lambda: self.stick.getY()))
         self.limelight.setDefaultCommand(limelightDisplay(self.limelight))
+        
+        
+
+
+
+
+
